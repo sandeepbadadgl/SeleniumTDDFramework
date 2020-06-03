15e888e4a3392b98acd4ccef71c62b5daf018937
sandeepbadadgl <sandeep.badadg@globallogic.com>
2020-06-03 15:12:34 +0530

    first commit

---
+++ src/test/java/com/qa/test/DemoTest.java
@@ -0,0 +1,28 @@
+package com.qa.test;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	
+		
+	@Test
+	public void testSum()	{
+		System.out.println("SUM");
+		int a= 2;
+		int b =2;
+		Assert.assertEquals(4, a+b);
+	}
+	
+	@Test
+	public void testMultiply()	{
+		System.out.println("MUL");
+		int a= 2;
+		int b =2;
+		Assert.assertEquals(4, a+b);
+	}
+	
+	
+}
